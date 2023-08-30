c37523419e677649e29ff4729eaf357f8e2addd3
Savannah Vaithilingam <savannah.vaithilingam@aexp.com>
2023-08-30 14:47:12 +0100

    feat(states): understand states and hoisting states

--- app/src/main/java/com/example/basicscodelab_compose/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab_compose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,6 +17,11 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,7 +42,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(names: List<String> = listOf("World", "Compose")){
+fun MyApp() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    // Logic to show onboarding or greetings
+    if(shouldShowOnboarding){
+        OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+    }else{
+        Greetings()
+    }
+}
+
+@Composable
+fun Greetings(names: List<String> = listOf("World", "Compose")){
     // A surface container using the 'background' color from the theme
     Surface(color = MaterialTheme.colorScheme.background) {
         Column(modifier = Modifier.padding(vertical = 4.dp)) {
@@ -49,6 +66,8 @@ fun MyApp(names: List<String> = listOf("World", "Compose")){
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember { mutableStateOf(false) }
+    var extraPadding = if(expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
@@ -56,14 +75,53 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         Row (modifier = Modifier.padding(24.dp)){
             Column( modifier = Modifier
                 .weight(1f) // flexible child, whilst the button isn't
+                .padding(bottom = extraPadding)
             ){
                 Text(text = "Hello," )
                 Text(text = name)
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text("Show More")
+            Button(onClick = { expanded.value = !expanded.value }) {
+                Text(if(expanded.value) "Show Less" else "Show More")
             }
         }
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    // Pass a function/callback
+    onContinueClicked: () -> Unit
+){
+//    //  TODO: Hoisting to MyApp()
+//    // USing by we are using a delegate in comparison to L58. we don't need the .value
+//    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            // center children in the column
+            verticalArrangement = Arrangement.Center,
+            // every child in the column will be centered
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to the Basics CodeLab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                // function call back, which will execute when it's clicked.
+//                onClick = { shouldShowOnboarding = false}
+                onClick = onContinueClicked
+            ){
+                Text(text = "Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    BasicsCodelabComposeTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
 
